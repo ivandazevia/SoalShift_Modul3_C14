aa8653d8e7c2eb50816cfd4470ec48e09607cb14
ivandazevia <zevi2308@gmail.com>
2017-10-25 08:53:03 +0700

    final sepertinya

--- soal3.c
+++ soal3.c
@@ -12,9 +12,9 @@ int lohan = 100, kepiting = 100;
 
 void* kepiting_lapar (void *arg){
     while(kepiting>0){
-        sleep(10);
+        sleep(20);
         kepiting-=10; 
-        printf("kepiting : %d\n",kepiting);
+        //printf("kepiting : %d\n",kepiting);
         if(kepiting<=0){
             printf("Kepiting mati :(\n");
             break;
@@ -23,9 +23,9 @@ void* kepiting_lapar (void *arg){
 }
 void* lohan_lapar (void *arg){
     while(lohan>0){
-        sleep(5);
+        sleep(10);
         lohan-=15;
-        printf("lohan : %d\n",lohan);
+        //printf("lohan : %d\n",lohan);
         if(lohan<=0){
             printf("Lohan mati :(\n");
             break;       
@@ -52,5 +52,23 @@ void* lohan_makan (void *arg){
 }
 
 int main(){
-    
+    int input;
+    pthread_create(&(tid1),NULL,&kepiting_lapar,NULL);
+    pthread_create(&(tid2),NULL,&lohan_lapar,NULL);
+    printf("Apa yang ingin anda lakukan?\n");
+    printf("1. Beri Makan Kepiting\n");
+    printf("2. Beri Makan Lohan\n");
+    while(1){
+        printf("Masukkan pilihan anda (1/2): ");
+        scanf("%d",&input);
+        if(input == 1){
+            pthread_create(&(tid3),NULL,&kepiting_makan,NULL); 
+            //printf("kepiting : %d\n",kepiting);
+        }
+        else if(input == 2){ 
+            pthread_create(&(tid4),NULL,&lohan_makan,NULL); 
+            //printf("lohan : %d\n",lohan);
+        }
+        printf("Kepiting = %d\nLohan = %d\n",kepiting, lohan);
+    }
 }
