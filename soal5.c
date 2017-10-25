365ccfecb030dd4dfbb8944f8f8844c0d528a12c
ivandazevia <zevi2308@gmail.com>
2017-10-25 09:27:47 +0700

    fungsi fina

---
+++ soal5.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+pthread_t tid1;
+pthread_t tid2;
+
+void* fina(void *arg){
+    FILE *fileinput;
+    fileinput = fopen("/home/zevi/SoalShift_Modul3_C14/Novel.txt","r");
+    char tmp[6000];
+    int jumlahfina=0;
+    fgets(tmp, sizeof(tmp),fileinput);
+    if(strstr(tmp,"Fina")){
+        jumlahfina++;
+    }
+    fclose(fileinput);
+    printf("Fina = %d\n",jumlahfina);
+}
+
+
+
