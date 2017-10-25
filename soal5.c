583fac4a241313395f99416500d20387034349b9
ivandazevia <zevi2308@gmail.com>
2017-10-25 10:05:12 +0700

    sepertinya sudah

--- soal5.c
+++ soal5.c
@@ -6,28 +6,39 @@
 
 pthread_t tid1;
 pthread_t tid2;
+int status;
+int jumlahfina=0;
+int jumlahifah=0;
 
 void* fina(void *arg){
+    status = 0;
     FILE *fileinput;
     fileinput = fopen("/home/zevi/SoalShift_Modul3_C14/Novel.txt","r");
-    char tmp[6000];
-    int jumlahfina=0;
-    fgets(tmp, sizeof(tmp),fileinput);
-    if(strstr(tmp,"Fina")){
-        jumlahfina++;
+    char tmp[400];
+    
+    while(fileinput!=NULL && fgets(tmp, sizeof(tmp), fileinput)!=NULL){
+        //fgets(tmp, sizeof(tmp),fileinput);
+        if(strstr(tmp,"Fina")){
+            //printf("Fina\n");
+            jumlahfina++;
+        }
     }
     fclose(fileinput);
     printf("Fina = %d\n",jumlahfina);
+    status =1;
 }
 
 void* ifah(void *arg){
+    while(status!=1){}
     FILE *fileinput;
     fileinput = fopen("/home/zevi/SoalShift_Modul3_C14/Novel.txt","r");
-    char tmp[6000];
-    int jumlahifah=0;
-    fgets(tmp, sizeof(tmp),fileinput);
-    if(strstr(tmp,"Ifah")){
-        jumlahifah++;
+    char tmp[400];
+    
+    while(fileinput!=NULL && fgets(tmp, sizeof(tmp), fileinput)!=NULL){
+        if(strstr(tmp,"Ifah")){
+            //printf("Ifah\n");
+            jumlahifah++;
+        }
     }
     fclose(fileinput);
     printf("Ifah = %d\n",jumlahifah);
