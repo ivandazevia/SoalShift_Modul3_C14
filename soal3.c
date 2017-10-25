2e98faf4b33da53048d8fb6ff337698ee89ba08a
ivandazevia <zevi2308@gmail.com>
2017-10-25 08:45:43 +0700

    belum ada int main, tapi semua fungsi sudah

--- soal3.c
+++ soal3.c
@@ -8,24 +8,48 @@ pthread_t tid1;
 pthread_t tid2;
 pthread_t tid3;
 pthread_t tid4;
-int status;
 int lohan = 100, kepiting = 100;
 
 void* kepiting_lapar (void *arg){
-    while(1){
-        sleep(20);
+    while(kepiting>0){
+        sleep(10);
         kepiting-=10; 
-        if(kepiting<=0) printf("Kepiting mati :(\n");
+        printf("kepiting : %d\n",kepiting);
+        if(kepiting<=0){
+            printf("Kepiting mati :(\n");
+            break;
+        }
     }
 }
 void* lohan_lapar (void *arg){
-    while(1){
-        sleep(10);
+    while(lohan>0){
+        sleep(5);
         lohan-=15;
-        if(lohan<=0) printf("Lohan mati :(\n");
+        printf("lohan : %d\n",lohan);
+        if(lohan<=0){
+            printf("Lohan mati :(\n");
+            break;       
+        }
     }
 }
 
+void* kepiting_makan (void *arg){
+    kepiting+=10;
+    if(kepiting>100){
+        //printf("kepiting : %d\n",kepiting);
+        printf("Kepiting mati kegendutan :(\n");
+        exit(0);
+    }
+}
+
+void* lohan_makan (void *arg){
+    lohan+=10;
+    if(lohan>100){
+        //printf("lohan : %d\n",lohan);
+        printf("Lohan mati kegendutan :(\n"); 
+        exit(0);
+    }
+}
 
 int main(){
     
