0a26c1ad48bfca7456b499d20726afe886b6aa3f
ivandazevia <zevi2308@gmail.com>
2017-10-25 17:56:34 +0700

    lebih baik

--- soal2.c
+++ soal2.c
@@ -14,14 +14,13 @@ pthread_t tid1,tid2;
 
 void* pemain1milih(void *arg){
     status = 0;
-    int lubang,jml;
+    int lubang;
+    
     printf("Giliran %s untuk bermain\n",pemain1);
-    printf("Ada berapa banyak ranjau yang ingin anda letakkan?\n");
-    scanf("%d",&jml);
-    printf("Silahkan masukkan 1-%d ranjau pada lubang 1-16\n",jml);
-    for(int i=1;i<=jml;i++){
+    printf("Silahkan masukkan 1-4 ranjau pada lubang 1-16\n");
+    for(int i=1;i<=4;i++){
         scanf("%d",&lubang);    
-        if(ranjau[1][i]!=0) printf("lubang sudah terisi\n");
+        if(ranjau[1][i]==1) printf("lubang sudah terisi\n");
         else {
             ranjau[1][i] = 1;
             jmllubang++;
@@ -32,8 +31,8 @@ void* pemain1milih(void *arg){
     printf("Silahkan %s untuk menebak 4 lubang yang aman\n",pemain2);
     for(int i=1;i<=4;i++){
         scanf("%d",&lubang);
-        if(ranjau[1][i]==1) score1++;
-        else score2++;
+        if(ranjau[1][i]==1){score1++; printf("score1: %d\n",score1);}
+        else{score2++; printf("score2: %d\n",score2);}
     }
     status = 1;
 }
@@ -42,12 +41,10 @@ void* pemain2milih(void *arg){
     while(status!=1){};
     int lubang,jml;
     printf("Giliran %s untuk bermain\n",pemain2);
-    printf("Ada berapa banyak ranjau yang ingin anda letakkan?\n");
-    scanf("%d",&jml);
-    printf("Silahkan masukkan 1-%d ranjau pada lubang 1-16\n",jml);
-    for(int i=1;i<=jml;i++){
+    printf("Silahkan masukkan 1-4 ranjau pada lubang 1-16\n");
+    for(int i=1;i<=4;i++){
         scanf("%d",&lubang);    
-        if(ranjau[2][i]!=0) printf("lubang sudah terisi\n");
+        if(ranjau[2][i]==1) printf("lubang sudah terisi\n");
         else {
             ranjau[2][i] = 1;
             jmllubang++;        
@@ -58,8 +55,8 @@ void* pemain2milih(void *arg){
     printf("Silahkan %s untuk menebak 4 lubang yang aman\n",pemain1);
     for(int i=1;i<=4;i++){
         scanf("%d",&lubang);
-        if(ranjau[2][i]==1) score2++;
-        else score1++;
+        if(ranjau[2][i]==1) {score2++; printf("score2: %d\n",score2);}
+        else {score1++;printf("score1: %d\n",score1);}
     }
 }
 
@@ -73,17 +70,19 @@ int main(){
 	scanf("%s",pemain2);
     getchar();
     printf("%s anda adalah pemain 2\n\n",pemain2);
-    
-    while(1){
-        for(int i=1;i<=16;i++){
+    for(int i=1;i<=16;i++){
             ranjau[1][i]=0;
             ranjau[2][i]=0;        
-        }  
+    }  
+    while(1){
+
         pthread_create(&(tid1),NULL,&pemain1milih,NULL); 
         pthread_create(&(tid2),NULL,&pemain2milih,NULL);
         
         pthread_join(tid1, NULL);
-    	pthread_join(tid2, NULL); 
+        pthread_join(tid2, NULL); 
+
+
         if(score1==10 || score2 == 10){
             printf("Game berakhir karena salah satu pemain sudah mendapatkan 10 poin\n");
             exit(0);
